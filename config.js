f61ee9f21f567728bcb3f596910615c0a1f05984
karnan <karnan.se@gmail.com>
2025-04-09 17:56:02 +0530

    backend

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const ConfigKeys = {
   mongoURL: process.env.MONGOURL,
   PORTNUMBER: process.env.PORT_NUMBER,
-  CLIENT_ORGIN: process.env.NODE_ENV == "development" ? "http://localhost:5173" : "",
+  CLIENT_ORGIN: process.env.NODE_ENV == "development" ? "http://localhost:5173" : "https://jiitak-japaneese-i3p4hsibs-karnan-ses-projects.vercel.app",
   ACCESS_TOKEN_EXPIRES_IN: "15m",
   REFRESH_TOKEN_EXPIRES_IN: "7d",
   JWTSECRET :process.env.JWTSECRET,
